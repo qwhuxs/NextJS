e54cd3e8408cfaadf684f810627d625ac56fe86f
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2026-04-29 15:54:23 +0300

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-
   retries: 2,
-
+  reporter: "html",
   use: {
     baseURL: "http://localhost:3000",
     headless: true,
